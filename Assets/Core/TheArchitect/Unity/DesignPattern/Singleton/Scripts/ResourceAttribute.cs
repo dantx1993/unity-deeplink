41c27f1473d8f0d79a4d16bbc7a3bb75547c6e99
dantx <dan.tx.work@gmail.com>
2025-06-27 11:48:49 +0700

    - Added: Core

---
+++ Assets/Core/TheArchitect/Unity/DesignPattern/Singleton/Scripts/ResourceAttribute.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace TheArchitect.Attributes
+{
+    /// <summary>
+    /// Attribute For Define Resourse Prefab Name and DontDestroyOnLoad flag
+    /// </summary>
+    public class ResourceAttribute : Attribute
+    {
+        // Flag for check Dont Destroy on Load
+        private bool _isDontDestroyOnLoad = false;
+        public bool IsDontDestroyOnLoad => _isDontDestroyOnLoad;
+
+        // Name of Resource Prefab
+        private string _name;
+        public string Name => _name;
+
+        // Contructor
+        public ResourceAttribute()
+        {
+            _name = "";
+        }
+        public ResourceAttribute(string name)
+        {
+            _name = name;
+        }
+        public ResourceAttribute(bool isDontDestroyOnLoad) : base()
+        {
+            _isDontDestroyOnLoad = isDontDestroyOnLoad;
+        }
+        public ResourceAttribute(string name, bool isDontDestroyOnLoad)
+        {
+            _name = name;
+            _isDontDestroyOnLoad = isDontDestroyOnLoad;
+        }
+    }
+}
